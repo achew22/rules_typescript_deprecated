3d9336fc49dbd51b31901ccf7cf0cd42487b8d75
Andrew Z Allen <me@andrewzallen.com>
2016-08-15 23:46:12 -0600

    Update run script to run only the first nar

--- typescript/run.sh
+++ typescript/run.sh
@@ -1,20 +1,21 @@
 #! /usr/bin/env bash
 
 set -e
+set -x
 
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 echo "$OS"
 
+find . | grep "typescript-.*-${OS}-x64\.nar"
 
 # There is probably a better way to do this, but this turns out to be a fiarly
 # robust way to target the .nar execuatable.
-NAR="$(find . | grep "typescript-.*-${OS}-x64\.nar")"
-echo "$NAR"
+NAR="$(pwd)/$(find . | grep "typescript-.*-${OS}-x64\.nar" | head -n 1)"
 
 # There is a race condition when compiling multiple ts_binaries. mkdir is
 # atomic and has an error code when it can't make a file (making it a perfect
 # lock). Lock on the .nar folder until .tsc_unlock is available.
-if mkdir .nar 2> /dev/null; then
+if mkdir .nar ; then
   until [[ -f .tsc_unlock ]]; do
     sleep .1s
   done
